39e59e7de90113ae2980e4dee1f4407b9d74e448
HontarievIKM724v <Maksym.Hontariev@infiz.khpi.edu.ua>
2025-04-04 19:47:41 +0200

    Add project files.

---
+++ LB11.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	char A, B;
+
+	cout << "Enter the value of the character variable A: ";
+	cin >> A;
+	cout << "\n Enter the value of the character variable B: ";
+	cin >> B;
+
+	char* pA = &A;
+	char* pB = &B;
+	cout << "\n Values of the entered variables:";
+	cout << "\n \t Variable A: " << A;
+	cout << "\n \t Variable B: " << B;
+
+	*pA = 'N';
+	cout << "\nChanging the value of variable A through a pointer: \t \nVariable A : " << *pA;
+
+	char temp = *pA;
+	*pA = *pB;
+	*pB = temp;
+
+	cout << "\n Values of the variables after changing the values:";
+	cout << "\n \t Variable A: " << A;
+	cout << "\n \t Variable B: " << B;
+
+}
